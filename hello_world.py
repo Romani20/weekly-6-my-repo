82f68bdb15b6a365ed153ec09e57706cdbd4aebe
rosbourne <rosbourne@wesleyan.edu>
2023-10-13 15:31:32 -0400

    Program to get the current state of the world.

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+def getTheStateOfTheWorld(array):
+    a = ''
+    if array.count("war") > array.count("peace"):
+        a = "Bye bye world"
+    else:
+        a = "Hello world"
+    return a
+    
+situation = ["war", "war"]
+print(getTheStateOfTheWorld(situation))
